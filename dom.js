8aefebf5705f824b7acfdf0151135a3c21d9f2b3
KULDEEP-18102000 <jadonkuldeepsingh2@gmail.com>
2022-12-21 15:54:32 +0530

    For task-4 for dom manipulation

--- dom.js
+++ dom.js
@@ -26,3 +26,16 @@ add_item=document.querySelector('.title');
 console.log(add_item);
 add_item.style.fontWeight='bold';
 add_item.style.color='green';
+
+var items=document.getElementsByClassName('list-group-item');
+console.log(items);
+console.log(items[1]);
+// items[1].textContent='Hello';
+// items[1].style.fontWeight='bold';
+// items[1].style.backgroundColor='yellow';
+
+items[2].style.backgroundColor='green';
+
+for(var i=0;i<items.length;i++){
+    items[i].style.fontWeight='bold';
+}
