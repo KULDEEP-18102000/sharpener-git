bdd6144d3ec50a23955c6c9f2a11a8476b3111f4
KULDEEP-18102000 <jadonkuldeepsingh2@gmail.com>
2022-12-21 14:05:17 +0530

    adding dom file

---
+++ dom.js
@@ -0,0 +1,28 @@
+// console.dir(document);
+// console.log(document.domain);
+// console.log(document.URL);
+// console.log(document.title);
+// // console.log(document.title=123);
+// console.log(document.doctype);
+// console.log(document.head);
+// console.log(document.body);
+// // console.log(document.all);
+// // console.log(document.all[10]);
+// // document.all[10]='Hello';
+// console.log(document.forms[0]);
+// console.log(document.links);
+
+// console.log(document.getElementById('header-title'));
+var headerTitle=document.getElementById('header-title');
+var header=document.getElementById('main-header');
+// console.log(headerTitle);
+// headerTitle.textContent='Hello';
+// headerTitle.innerText='GoodBye';
+console.log(headerTitle.textContent);
+console.log(headerTitle.innerText);
+// headerTitle.innerHTML='<h3>hello</h3>';
+headerTitle.style.border='solid 3px #000';
+add_item=document.querySelector('.title');
+console.log(add_item);
+add_item.style.fontWeight='bold';
+add_item.style.color='green';
